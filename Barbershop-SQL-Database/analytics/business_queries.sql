2605ca6d6973bde41553a70e5a94a699606667eb
djon-sql <stivjob06@gmail.com>
2026-05-01 20:34:50 +0300

    Update business_queries.sql

--- Barbershop-SQL-Database/analytics/business_queries.sql
+++ Barbershop-SQL-Database/analytics/business_queries.sql
@@ -5,58 +5,78 @@
 
 --Топ барбери по доходу
 SELECT 
-    BarberId,
-    SUM(TotalPrice) AS TotalRevenue
-FROM VisitArchive
-GROUP BY BarberId
+    b.Id,
+    b.FirstName + ' ' + b.LastName AS BarberName,
+    COUNT(va.Id) AS TotalVisits,
+    SUM(va.TotalPrice) AS TotalRevenue,
+    AVG(va.TotalPrice) AS AvgCheck
+FROM VisitArchive va
+JOIN Barbers b ON va.BarberId = b.Id
+GROUP BY 
+    b.Id,
+    b.FirstName,
+    b.LastName
 ORDER BY TotalRevenue DESC
 
 --Найпопулярніші послуги
 SELECT 
     s.ServiceName,
-    COUNT(*) AS UsageCount
+    COUNT(*) AS UsageCount,
+    SUM(va.TotalPrice) AS RevenueGenerated
 FROM VisitServices vs
-JOIN Service_Price_Time s 
-    ON vs.ServiceId = s.Id
+JOIN Service_Price_Time s ON vs.ServiceId = s.Id
+JOIN VisitArchive va ON vs.VisitId = va.Id
 GROUP BY s.ServiceName
 ORDER BY UsageCount DESC
 
 --Середній чек
 SELECT 
-    AVG(TotalPrice) AS AvgCheck
+    ROUND(AVG(TotalPrice), 2) AS AvgCheck
 FROM VisitArchive
 
 --Активні клієнти (топ)
 SELECT TOP 10
-    ClientId,
-    COUNT(*) AS Visits
-FROM VisitArchive
-GROUP BY ClientId
-ORDER BY Visits DESC
+    c.Id,
+    c.FirstName + ' ' + c.LastName AS ClientName,
+    COUNT(va.Id) AS TotalVisits,
+    SUM(va.TotalPrice) AS TotalSpent
+FROM VisitArchive va
+JOIN Clients c ON va.ClientId = c.Id
+GROUP BY 
+    c.Id,
+    c.FirstName,
+    c.LastName
+ORDER BY TotalVisits DESC
 
 --Завантаженість барберів
 SELECT 
-    BarberId,
-    COUNT(*) AS AppointmentsCount
-FROM Appointments
-GROUP BY BarberId
+    b.Id,
+    b.FirstName + ' ' + b.LastName AS BarberName,
+    COUNT(a.Id) AS AppointmentsCount
+FROM Appointments a
+JOIN Barbers b ON a.BarberId = b.Id
+GROUP BY 
+    b.Id,
+    b.FirstName,
+    b.LastName
 ORDER BY AppointmentsCount DESC
 
 --Дохід по днях
 SELECT 
-    VisitDate,
-    SUM(TotalPrice) AS DailyRevenue
+    CAST(VisitDate AS DATE) AS VisitDate,
+    SUM(TotalPrice) AS DailyRevenue,
+    COUNT(*) AS TotalVisits
 FROM VisitArchive
-GROUP BY VisitDate
+GROUP BY CAST(VisitDate AS DATE)
 ORDER BY VisitDate
 
 --Retention (повернення клієнтів)
 SELECT 
-    COUNT(DISTINCT ClientId) AS ReturningClients
-FROM VisitArchive
-WHERE ClientId IN (
+    COUNT(*) AS ReturningClients
+FROM (
     SELECT ClientId
     FROM VisitArchive
     GROUP BY ClientId
-    HAVING COUNT(*) > 1)
+    HAVING COUNT(*) > 1) t
+
 
